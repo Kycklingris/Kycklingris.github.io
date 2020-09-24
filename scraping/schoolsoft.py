f16551e2d30fd27b02b8b9305604a03d582fcef2
Malte Linde Neveling <malte.linde.neveling@gmail.com>
2020-09-24 15:18:59 +0200

    Move to desktop

--- scraping/schoolsoft.py
+++ scraping/schoolsoft.py
@@ -3,15 +3,20 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import json
+import time
 
 from selenium.webdriver.chrome.options import Options
 
-import keys
-
-options = Options()
-options.headless = True
+import keys  #To use, make your own keys.py file with variables username and password
 
 path = os.getcwd()
+
+options = Options()
+#options.binary_location = path + "/scraping/Chrome/Chrome.exe"
+options.headless = True
+options.disable_extensions = True
+options.add_argument ('--no-sandbox')
+
 
 
 driver = webdriver.Chrome(executable_path = path + "/scraping/chromedriver.exe", options = options)
@@ -31,8 +36,37 @@ btn.click()
 
 
 driver.get("https://sms.schoolsoft.se/nti/jsp/student/right_student_news.jsp?menu=news")
-html = driver.page_source
+
+
+
+html=driver.page_source
+
+
+
+driver.stop_client()
+driver.close()
 driver.quit()
 
-soup = BeautifulSoup(html, "lxml")
-print(soup.title.text)
+print(soup.find(id=''))
+
+
+"""
+soup = BeautifulSoup(html, "HTML.parser")
+print(soup.find(id='accordion-group69500'))
+
+
+main = soup.find(id='main')
+news = main.find(id='news_con_content')
+
+accordions = news.findAll("div", {"class": "accordion"})
+
+lunchdiv = accordions[5]
+
+accordioninner = lunchdiv.find(id='accordion-inner69500')
+
+lunchlista = accordioninner.find("div", {"class": "accordion_inner_left"})
+
+resturanger = lunchlista.findAll('p', {"class": "tinymce-p"})
+
+print(lunchlista)
+
