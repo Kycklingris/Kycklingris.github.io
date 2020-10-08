165af7421f9ae9bcb811b9fb07a02c17d68dcede
Malte Linde Neveling <malte.linde.neveling@gmail.com>
2020-10-08 16:14:35 +0200

    Removed mandatory stuff, webscraping done, map v1

--- main.js
+++ main.js
@@ -1,149 +1,58 @@
-var data = [
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "4.5/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "10/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway7",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  },
-  {
-    name: "Subway",
-    location: "Odenplan",
-    review: "7/10",
-    annexet: "5min",
-    craford: "10min",
-  }
-  
-];
+var response = fetch("./data.json")
+
+var data = JSON.parse(response)
+
+
+
+
+data.forEach(res => {
+  var value = Math.floor(Math.random() * 10);
+  res["review"] = value;
+});
 
 var id = 1;
 
 data.forEach(res => {
-    let card = document.createElement("div");
+  let card = document.createElement("div");
   card.classList.add("card");
   card.id = "card" + parseInt(id);
 
-    let header = document.createElement("H2")
-    header.classList.add("header");
-    let name = document.createTextNode(res.name);
-    header.appendChild(name);
-    card.appendChild(header);
+  let header = document.createElement("H2")
+  header.classList.add("header");
+  let name = document.createTextNode(res.name);
+  header.appendChild(name);
+  card.appendChild(header);
 
 
-    let address = document.createElement("p");
-    address.classList.add("address");
-    let location = document.createTextNode(res.location);
-    address.appendChild(location);
-    card.appendChild(address);
+  let address = document.createElement("p");
+  address.classList.add("address");
+  let location = document.createTextNode(res.location);
+  address.appendChild(location);
+  card.appendChild(address);
 
-    let review = document.createElement("p");
-    review.classList.add("review");
-    let reviewtmp = document.createTextNode('review:' + res.review);
-    review.appendChild(reviewtmp);
-    card.appendChild(review);
+  let review = document.createElement("p");
+  review.classList.add("review");
+  let reviewtmp = document.createTextNode('review:' + res.review);
+  review.appendChild(reviewtmp);
+  card.appendChild(review);
 
   id++;
 
 
 
-    let container = document.querySelector("#container");
-    container.appendChild(card);
+  let container = document.querySelector("#container");
+  container.appendChild(card);
+});
+
+var map = new ol.Map({
+  target: "map",
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([18.04724, 59.340148]),
+    zoom: 15,
+  }),
 });
--- scraping/schoolsoft.py
+++ scraping/schoolsoft.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
 import os
 import json
 import time
+import copy
+import string
+import sys
+import codecs
+import googlemaps
+import responses
 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
@@ -19,8 +24,10 @@ options.disable_extensions = True
 options.add_argument('--no-sandbox')
 
 
-driver = webdriver.Chrome(executable_path=path +"/scraping/chromedriver.exe", options=options)
+driver = webdriver.Chrome(executable_path=path +
+                          "/scraping/chromedriver.exe", options=options)
 
+gmaps = googlemaps.Client(key=keys.apikey)
 
 USERNAME = keys.username
 PASSWORD = keys.password
@@ -31,10 +38,12 @@ btn = driver.find_elements_by_xpath("//*[@id='username']")[0]
 btn.send_keys(USERNAME)
 btn = driver.find_elements_by_xpath("//*[@id='password']")[0]
 btn.send_keys(PASSWORD)
-btn = driver.find_elements_by_xpath("//*[@class='form__button form__button--primary']")[0]
+btn = driver.find_elements_by_xpath(
+    "//*[@class='form__button form__button--primary']")[0]
 btn.click()
 
-driver.get("https://sms.schoolsoft.se/nti/jsp/student/right_student_news.jsp?menu=news")
+driver.get(
+    "https://sms.schoolsoft.se/nti/jsp/student/right_student_news.jsp?menu=news")
 time.sleep(2)
 
 
@@ -61,51 +70,148 @@ accordion = soup.find(id='accordion-inner69500')
 
 lunchlista = accordion.findAll("div", {"class": "accordion_inner_left"})
 
-resturanger = []
+tmpresturanger = []
 
-for i in lunchlista[0].find_all('p', {'class': "tinymce-p"})[6:]:
+lunchlista = lunchlista[0]
+
+for i in lunchlista.findAll('a', href=True):
+    i.extract()
+
+for i in lunchlista.find_all('p', {'class': "tinymce-p"})[6:]:
     if i.text == "\xa0":
         continue
-    resturanger.append(i.text)
+    tmpresturanger.append(i.text)
 
-resturanger = resturanger[:-1]
 
-resturanger2 = []
-for i in resturanger:
+new = False
+old = []
+
+with codecs.open('scraping/old.txt', 'r', encoding="utf8") as f:
+    for i in f:
+        tmp = i[:-1]
+        old.append(tmp)
+
+if tmpresturanger == old:
+    new = False
+else:
+    new = True
+
+
+with codecs.open('scraping/old.txt', 'w', encoding="utf8") as f:
+    for i in tmpresturanger:
+        f.writelines("%s\n" % i)
+
+# use if bypassing previous check is needed.
+new = True
+
+if new == False:
+    sys.exit()
+
+tmpresturanger = tmpresturanger[:-1]
+
+tmpresturanger2 = []
+for i in tmpresturanger:
+    i = i.lower()
     i = i.translate({ord('*'): None})
+    i = i.translate({ord('.'): None})
     i = i.translate({ord("'"): None})
     i = i.translate({ord('#'): None})
     i = i.translate({ord('´'): None})
     i = i.translate({ord(":"): None})
     i = i.translate({ord(";"): None})
     i = i.translate({ord("!"): None})
-    i = i.replace('STOCKHOLM', '')
-    i = i.replace('Stockholm', '')
     i = i.replace('stockholm', "")
-    i = i.replace('sTOCKHOLM', '')
     i = i.replace(' - ', ',')
-    resturanger2.append(i)
+    i = i.replace(' ny', ' tmp  ny')
+    i = i.replace(' tillbaka', ' tmp  tillbaka')
 
-resturanger3 = []
+    tmpresturanger2.append(i)
 
+failure = True
+resturanger = []
 
-class resturang(object):
-    def __init__(self, name = 'N/A', location = 'N/A', annexet = 'N/A', craford = 'N/A', extra = 'N/A', review =  'N/A', description = 'N/A'):
-        self.name = name
-        self.location = location
-        self.annexet = annexet
-        self.craford = craford
-        self.extra = extra
-        self.review = review
-        self.description = description
-
-for i in resturanger2:
+for i in tmpresturanger2:
     tmp = i.split(',', 1)
-    tmp2 = tmp[1].split(' NY', 1)
-    tmp2 = tmp[1].split(' ny', 1)
-    tmp2 = tmp[1].split(' Ny', 1)
-    tmp2 = tmp[1].split(' Tillbaka', 1)
-    tmp2 = tmp[1].split(' tillbaka', 1)
-    tmp2 = tmp[1].split(' TILLBAKA', 1)
+    if len(tmp) == 1:
+        continue
+
+    tmp2 = tmp[1]
+    tmp3 = tmp2.split(' tmp ', 1)
+
+    name = tmp[0]
+    if len(tmp3) == 2:
+        location = tmp3[0]
+        extra = tmp3[1]
+    else:
+        location = tmp[1]
+        extra = ""
+
+    while True:
+        if name.startswith(" ") or name.startswith(","):
+            name = name[1:]
+        elif name.endswith(" ") or name.endswith(","):
+            name = name[:-1]
+        elif "/" in name:
+            tmpname = name.split("/", 1)
+            name = tmpname[0]
+        elif location.startswith(" ") or location.startswith(","):
+            location = location[1:]
+        elif location.endswith(" ") or location.endswith(","):
+            location = location[:-1]
+        elif extra.startswith(" ") or extra.startswith(","):
+            extra = extra[1:]
+        elif extra.endswith(" ") or extra.endswith(","):
+            extra = extra[:-1]
+        else:
+            break
+
+    name = string.capwords(name)
+    location = string.capwords(location)
+    extra = string.capwords(extra)
+    while True:
+        try:
+            geocode_result = gmaps.geocode(location + ' Stockholm')
+        except:
+            time.sleep(2)
+            continue
+        else:
+            break
+
+    street_number = geocode_result[0]["address_components"][0]["short_name"]
+    street_name = geocode_result[0]["address_components"][1]["short_name"]
+
+    mapsaddress = street_name + " " + street_number
+
+    latitude = geocode_result[0]["geometry"]["location"]["lat"]
+    longitude = geocode_result[0]["geometry"]["location"]["lng"]
+
+    a = False
+    b = False
+
+    while a == False and b == False:
+        try:
+            directions_result_annexet = gmaps.directions(mapsaddress + "," + "Stockholm", "59.341667,18.049201", mode="walking")
+        except:
+            time.sleep(2)
+            continue
+        else:
+            a = True
+        try:
+            directions_result_craford = gmaps.directions(mapsaddress + "," + "Stockholm", "59.337579,18.046180", mode="walking")
+        except:
+            time.sleep(2)
+            continue
+        else:
+            b = True
+        
+
+    walking_time_annexet = directions_result_annexet[0]["legs"][0]['duration']['text']
+    walking_time_craford = directions_result_craford[0]["legs"][0]['duration']['text']
 
 
+    tmpdict = {"name": name, "location": mapsaddress, "extra": extra, "lon": longitude,"lat": latitude, "annexet": walking_time_annexet, "craford": walking_time_craford}
+    resturanger.append(copy.deepcopy(tmpdict))
+
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(resturanger, f, ensure_ascii=False, indent=4)
